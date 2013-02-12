4d49ac7b7113c5614374f814f27d80c4c0bc6a26
Paul Dowman <paul@pauldowman.com>
2013-02-12 14:13:08 -0500

    Add HTML header and footer

--- html_generator.rb
+++ html_generator.rb
@@ -1,9 +1,26 @@
 class HtmlGenerator
   def index
+    print_header
     puts "HtmlGenerator: index"
+    print_footer
   end
 
   def show
+    print_header
     puts "HtmlGenerator: show"
+    print_footer
+  end
+
+  def print_header
+    puts "<html>"
+    puts "  <head>"
+    puts "    <title>Connoisseur</title>"
+    puts "  </head>"
+    puts "  <body>"
+end
+
+  def print_footer
+    puts "  </body>"
+    puts "</html>"
   end
 end

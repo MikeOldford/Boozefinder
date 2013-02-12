c111d19c3dff29c7bb3faef381f3a41a46a8b8bd
Paul Dowman <paul@pauldowman.com>
2013-02-12 16:23:45 -0500

    Print some details for each product

--- html_generator.rb
+++ html_generator.rb
@@ -13,7 +13,16 @@ class HtmlGenerator
     # Loop over the products, printing each one
     products.each do |product|
       puts "<div class='product'>"
-      puts product["name"]
+      puts "  <h2>#{product['name']}</h2>"
+      puts "  <img src='#{product['image_thumb_url']}'  class='product-thumbnail'/>"
+      puts "  <ul class='product-data'>"
+      puts "    <li>id: #{product['id']}</li>"
+      puts "    <li>#{product['producer_name']}</li>"
+      puts "    <li>#{product['primary_category']}</li>"
+      puts "    <li>#{product['secondary_category']}</li>"
+      puts "    <li>#{product['volume_in_milliliters']} ml</li>"
+      puts "    <li>$#{format_price(product['price_in_cents'])}</li>"
+      puts "  </ul>"
       puts "</div>"
     end
     
@@ -54,6 +63,12 @@ class HtmlGenerator
 
     # Return just the actual result data from the response, ignoring metadata
     result = parsed_response["result"]
+  end
+
+  # Convert a string of a price in cents to a float.
+  # e.g. "199" to 1.99
+  def format_price(cents_string)
+    cents_string.to_f/100
   end
 
 end

b75fb3d5be64d47f9b1a8099cc8151c037844c55
Paul Dowman <paul@pauldowman.com>
2013-02-12 14:27:49 -0500

    Retrieve the list of products from the external API and print each

--- html_generator.rb
+++ html_generator.rb
@@ -1,7 +1,19 @@
+require 'json'
+require 'open-uri'
+
 class HtmlGenerator
   def index
+    # Retrieve the list of products
+    products = retrieve_data("http://lcboapi.com/products")
+
     print_header
-    puts "HtmlGenerator: index"
+
+    # Loop over the products, printing each one
+    products.each do |product|
+      puts product["name"]
+      puts "<br/>"
+    end
+    
     print_footer
   end
 
@@ -17,10 +29,22 @@ class HtmlGenerator
     puts "    <title>Connoisseur</title>"
     puts "  </head>"
     puts "  <body>"
-end
+  end
 
   def print_footer
     puts "  </body>"
     puts "</html>"
   end
+
+  def retrieve_data(url)
+    # Retrieve JSON-formatted text from lcboapi.com
+    raw_response = open(url).read
+
+    # Parse JSON-formatted text into a Ruby Hash
+    parsed_response = JSON.parse(raw_response)
+
+    # Return just the actual result data from the response, ignoring metadata
+    result = parsed_response["result"]
+  end
+
 end
